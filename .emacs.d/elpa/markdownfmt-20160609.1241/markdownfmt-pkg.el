602d4437c23261a4dd836f99f790bd351e521656
SekiroCC <sekirocc@gmail.com>
2022-05-13 09:44:07 +0800

    update

---
+++ .emacs.d/elpa/markdownfmt-20160609.1241/markdownfmt-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from markdownfmt.el  -*- no-byte-compile: t -*-
+(define-package "markdownfmt" "20160609.1241" "Format markdown using markdownfmt" '((emacs "24")) :commit "187a74eb4fd9e8520ce08da42d1d292b9af7f2b7" :authors '(("Nicolas Lamirault" . "nicolas.lamirault@gmail.com")) :maintainer '("Nicolas Lamirault" . "nicolas.lamirault@gmail.com") :keywords '("markdown") :url "https://github.com/nlamirault/emacs-markdownfmt")
