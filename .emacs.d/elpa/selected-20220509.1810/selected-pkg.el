3b3c0cb70930e861042b8478b8462e6aeb62a80b
Seki Rocc <sekirocc@gmail.com>
2022-06-11 20:51:12 +0800

    update

---
+++ .emacs.d/elpa/selected-20220509.1810/selected-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from selected.el  -*- no-byte-compile: t -*-
+(define-package "selected" "20220509.1810" "Keymap for when region is active" 'nil :commit "81cb32521a05ff2a9125e001b83608e108e480f6" :authors '(("Erik Sjöstrand")) :maintainer '("Erik Sjöstrand") :keywords '("convenience") :url "http://github.com/Kungsgeten/selected.el")
