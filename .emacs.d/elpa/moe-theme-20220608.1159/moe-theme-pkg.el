1ca917e360cbee73409138835a7b1c026e828850
ChenJie <chenjie1@sensetime.com>
2022-06-15 13:13:36 +0800

    update

---
+++ .emacs.d/elpa/moe-theme-20220608.1159/moe-theme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "moe-theme" "20220608.1159" "A colorful eye-candy theme. Moe, moe, kyun!" 'nil :commit "4d9e876d24d69d18c4111fa42bd34fe01a124866" :authors
+  '(("kuanyui" . "azazabc123@gmail.com"))
+  :maintainer
+  '("kuanyui" . "azazabc123@gmail.com")
+  :keywords
+  '("themes")
+  :url "https://github.com/kuanyui/moe-theme.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
