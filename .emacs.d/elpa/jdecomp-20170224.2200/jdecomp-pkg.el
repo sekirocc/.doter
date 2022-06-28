dd58bab694d18f4e2d45cdc7765de1ade15b5115
SekiroCC <sekirocc@gmail.com>
2022-06-28 20:32:09 +0800

    update

---
+++ .emacs.d/elpa/jdecomp-20170224.2200/jdecomp-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from jdecomp.el  -*- no-byte-compile: t -*-
+(define-package "jdecomp" "20170224.2200" "Interface to Java decompilers" '((emacs "24.5")) :commit "692866abc83deedce62be8d6040cf24dda7fb7a8" :authors '(("Tianxiang Xiong" . "tianxiang.xiong@gmail.com")) :maintainer '("Tianxiang Xiong" . "tianxiang.xiong@gmail.com") :keywords '("decompile" "java" "languages" "tools") :url "https://github.com/xiongtx/jdecomp")
---
+++ .emacs.d/lisp/download-lombok.el
@@ -0,0 +1,25 @@
+(require 'f)
+
+(defvar lsp-java-lombok--jar-path
+  (if (boundp 'doom-cache-dir)
+      (f-join doom-cache-dir "lombok/lombok.jar")
+    (f-join user-emacs-directory ".local/cache/lombok/lombok.jar")))
+
+(defun lsp-java-lombok-download ()
+  "Download the latest lombok jar."
+  (make-directory (f-dirname lsp-java-lombok--jar-path) t)
+  (when (f-exists-p lsp-java-lombok--jar-path)
+    (f-delete lsp-java-lombok--jar-path))
+  (lsp--info "Downloading lombok...")
+  (url-copy-file "https://projectlombok.org/downloads/lombok.jar" lsp-java-lombok--jar-path))
+
+(defun lsp-java-lombok-init ()
+  "Download lombok and set vmargs."
+  (unless (f-exists-p lsp-java-lombok--jar-path)
+    (lsp-java-lombok-download))
+)
+
+(add-hook 'java-mode-hook #'lsp-java-lombok-init)
+
+
+(provide 'download-lombok)
