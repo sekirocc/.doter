3377fca2a57231e33cbc5f170ade91307551e569
ChenJie <chenjie1@sensetime.com>
2022-06-28 10:36:36 +0800

    eglot

---
+++ .emacs.d/elpa/eglot-20220624.939/eglot-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from eglot.el  -*- no-byte-compile: t -*-
+(define-package "eglot" "20220624.939" "Client for Language Server Protocol (LSP) servers" '((emacs "26.1") (jsonrpc "1.0.14") (flymake "1.2.1") (project "0.3.0") (xref "1.0.1") (eldoc "1.11.0") (seq "2.23")) :commit "35a519a533c2d28e7d88e8541eca0de2daa531ee" :authors '(("João Távora" . "joaotavora@gmail.com")) :maintainer '("João Távora" . "joaotavora@gmail.com") :keywords '("convenience" "languages") :url "https://github.com/joaotavora/eglot")
