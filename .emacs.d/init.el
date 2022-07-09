a8a78af129ded99bdb26884be7e12086469f1006
Seki Rocc <sekirocc@gmail.com>
2022-07-09 12:51:46 +0800

    add region wrap

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,6 +38,8 @@
   (add-hook 'after-init-hook 'benchmark-init/deactivate))
 
 
+(require 'cl)
+
 
 ;; (setq  x-meta-keysym 'super
 ;;         x-super-keysym 'meta)
@@ -468,6 +470,9 @@
   (forward-line -1)
   (indent-according-to-mode))
 
+
+
+
 (use-package smartparens
   :ensure t
   :defer t
@@ -484,6 +489,59 @@
   ;;   ("C-c p s" . sp-rewrap-sexp)
   ;;  )
 )
+
+
+;;
+;; borrow from https://github.com/lujun9972/emacs-document/blob/master/emacs-common/Smartparens%E7%94%A8%E6%B3%95%E8%AF%A6%E8%A7%A3.org#key
+;;
+(defmacro def-region-pairs (pairs)
+  "Define functions for pairing. PAIRS is an alist of (NAME . STRING)
+conses, where NAME is the function name that will be created and
+STRING is a single-character string that marks the opening character.
+
+  (def-pairs ((paren . \"(\")
+              (bracket . \"[\"))
+
+defines the functions WRAP-WITH-PAREN and WRAP-WITH-BRACKET,
+respectively."
+  `(progn
+     ,@(loop for (key . val) in pairs
+             collect
+             `(defun ,(read (concat
+                             "my-wrap-region-with-"
+                             (prin1-to-string key)
+                             "s"))
+                  (start stop)
+                (interactive "r")
+                (sp-wrap-with-pair ,val)
+
+                (goto-char (+ 1 start))
+                (push-mark (+ 1 stop))
+                (setq deactivate-mark  nil)
+
+                ))))
+
+(def-region-pairs ((paren . "(")
+            (bracket . "[")
+            (brace . "{")
+            (single-quote . "'")
+            (double-quote . "\"")
+            (back-quote . "`")))
+(bind-keys :map smartparens-mode-map
+     ("C-c ("  . my-wrap-region-with-parens)
+     ("C-c ["  . my-wrap-region-with-brackets)
+     ("C-c {"  . my-wrap-region-with-braces)
+     ("C-c '"  . my-wrap-region-with-single-quotes)
+     ("C-c \"" . my-wrap-region-with-double-quotes)
+     ("C-c _"  . my-wrap-region-with-underscores)
+     ("C-c `"  . my-wrap-region-with-back-quotes))
+
+
+
+
+
+
+
 
 
 
@@ -836,7 +894,6 @@
 (require 'mwim)
 
 
-(require 'cl)
 
 
 
@@ -1832,6 +1889,16 @@ opening parenthesis one level up."
 
               ;; ("g g" . (lambda () (interactive) (beginning-of-buffer) (keyboard-quit) ))
               ;; ("G" .   (lambda () (interactive) (end-of-buffer) (keyboard-quit) ))
+
+
+              ("("  . my-wrap-region-with-parens)
+              ("["  . my-wrap-region-with-brackets)
+              ("{"  . my-wrap-region-with-braces)
+              ("'"  . my-wrap-region-with-single-quotes)
+              ("\"" . my-wrap-region-with-double-quotes)
+              ("_"  . my-wrap-region-with-underscores)
+              ("`"  . my-wrap-region-with-back-quotes)
+
         )
 )
 (selected-global-mode 1)
