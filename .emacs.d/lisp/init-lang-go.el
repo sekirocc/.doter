86011916003da7bad1caabb711edda8a6f3a192f
ChenJie <chenjie1@sensetime.com>
2022-07-21 18:26:00 +0800

    init go

---
+++ .emacs.d/lisp/init-lang-go.el
@@ -0,0 +1,28 @@
+
+;; init-lang-go.el --- Go Lang Config	-*- lexical-binding: t -*-
+
+;; Author: Zsxh Chen <bnbvbchen@gmail.com>
+;; URL: https://github.com/zsxh/emacs.d
+
+;;; Commentary:
+;;
+;;  Go Lang Config
+;;
+
+;;; Code:
+
+;; Install gopls
+;; https://github.com/golang/tools/blob/master/gopls/doc/user.md#installation
+(use-package go-mode
+  :mode ("\\.go\\'" . go-mode)
+  :hook (go-mode . eglot-ensure)
+  :config
+  (+eglot/set-leader-keys go-mode-map)
+  ;; Env vars
+  (with-eval-after-load 'exec-path-from-shell
+    (exec-path-from-shell-copy-envs '("GOPATH" "GO111MODULE" "GOPROXY"))))
+
+
+(provide 'init-lang-go)
+
+;;; init-lang-go.el ends here
