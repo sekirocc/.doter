8cfaa00231f31e9cea8ea5cea97543ad94fa3299
Seki Rocc <sekirocc@gmail.com>
2022-08-09 18:02:01 +0800

    upgrade packages

--- .emacs.d/elpa/cmake-mode-20210104.1831/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "0a6415ea1fa286346f119809323938a7cfca8565")
---
+++ .emacs.d/elpa/cmake-mode-20220617.1532/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20220617.1532" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "241fc839d56ccd666fe41269e291b8d8190cf97b")
