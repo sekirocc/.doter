b63ce9904c4c6ea7203eb5a6087949d3924f82ea
SekiroCC <sekirocc@gmail.com>
2022-08-09 18:02:01 +0800

    upgrade packages

---
+++ .emacs.d/elpa/helm-core-20220803.1447/helm-core-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm-core" "20220803.1447" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "3ce79a958a126623c2e3f65f6e90d7b4dd0248e8" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
