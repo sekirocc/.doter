f0402bcbc014ebf8a2f5bf9b1625ce026cbb8219
Seki Rocc <sekirocc@gmail.com>
2022-11-18 18:22:48 +0800

    update

--- .emacs.d/lisp/init-lang-zig.el
+++ .emacs.d/lisp/init-lang-zig.el
@@ -2,12 +2,12 @@
 ;;
 ;; hacks for zig-mode format.
 ;;
-(defun my-zig-mode-hook ()
-  (remove-hook 'before-save-hook 'zig-before-save-hook)
-  (add-hook 'after-save-hook 'zig-before-save-hook)
-  (add-hook 'after-revert-hook 'my-god-mode)
-)
-(add-hook 'zig-mode-hook 'my-zig-mode-hook)
+;; (defun my-zig-mode-hook ()
+;;   (remove-hook 'before-save-hook 'zig-before-save-hook)
+;;   (add-hook 'after-save-hook 'zig-before-save-hook)
+;;   (add-hook 'after-revert-hook 'my-god-mode)
+;; )
+;; (add-hook 'zig-mode-hook 'my-zig-mode-hook)
 
 ;; trigger eglot
 (add-hook 'zig-mode-hook 'eglot-ensure)
