37b402d466ce668644d2301e9eaf3ec1c11c3e64
Seki Rocc <sekirocc@gmail.com>
2022-12-21 20:31:19 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -462,7 +462,7 @@ local my_lsp_on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 
 
   -----   -- Set autocommands conditional on server_capabilities
