002fd113899eb0826928d063518d3c77311f724b
Seki Rocc <sekirocc@gmail.com>
2023-01-05 15:24:59 +0800

    add fish

--- fish/config.fish
+++ fish/config.fish
@@ -30,3 +30,66 @@ set -x PATH $PATH /opt/homebrew/opt/ruby/bin
 set -x PATH $PATH /opt/homebrew/lib/ruby/gems/3.1.0/bin
 
 
+
+
+
+
+
+####
+#### Git Alias
+####
+# Most used git command should be short.
+alias s='git status -sb'
+
+alias ga='git add -A'
+alias gap='ga -p'
+
+alias gbr='git branch -v'
+
+alias gch='git cherry-pick'
+
+alias gcm='git commit -v --amend'
+
+alias gco='git checkout'
+
+alias gd='git diff -M'
+alias gd.='git diff -M --color-words="."'
+alias gdc='git diff --cached -M'
+alias gdc.='git diff --cached -M --color-words="."'
+
+alias gf='git fetch'
+
+# Helper function.
+function git_current_branch
+  cat "$(git rev-parse --git-dir 2>/dev/null)/HEAD" | sed -e 's/^.*refs\/heads\///'
+end
+
+alias glog='git log --date-order --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
+alias gl='glog --graph'
+alias gla='gl --all'
+
+alias gm='git merge --no-ff'
+alias gmf='git merge --ff-only'
+
+alias gp='git push'
+alias gpthis='gp origin $(git_current_branch)'
+alias gpthis!='gp --set-upstream origin $(git_current_branch)'
+
+alias grb='git rebase -p'
+alias grba='git rebase --abort'
+alias grbc='git rebase --continue'
+alias grbi='git rebase -i'
+
+alias gr='git reset'
+alias grh='git reset --hard'
+alias grsh='git reset --soft HEAD~'
+
+alias grv='git remote -v'
+
+alias gs='git show'
+alias gs.='git show --color-words="."'
+
+alias gst='git stash'
+alias gstp='git stash pop'
+
+alias gup='git pull'
