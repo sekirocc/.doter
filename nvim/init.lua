3011334ba637d63835e7a0899652164648be1409
Seki Rocc <sekirocc@gmail.com>
2023-02-07 15:42:49 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -380,8 +380,8 @@ vim.g.AutoPairsShortcutJump = ''
 --
 
 vim.g.multi_cursor_use_default_mapping = 0
-vim.g.multi_cursor_start_word_key      = '<M-d>'
-vim.g.multi_cursor_next_key            = '<M-d>'
+vim.g.multi_cursor_start_word_key      = '<C-n>'
+vim.g.multi_cursor_next_key            = '<C-n>'
 vim.g.multi_cursor_skip_key            = '<C-x>'
 vim.g.multi_cursor_quit_key            = '<Esc>'
 
