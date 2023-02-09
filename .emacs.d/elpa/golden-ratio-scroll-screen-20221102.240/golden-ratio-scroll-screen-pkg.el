b147d309afc64dc1a04b6244642e244eb3e5dc4c
Seki Rocc <sekirocc@gmail.com>
2023-02-09 21:46:59 +0800

    upgrade packages

---
+++ .emacs.d/elpa/golden-ratio-scroll-screen-20221102.240/golden-ratio-scroll-screen-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from golden-ratio-scroll-screen.el  -*- no-byte-compile: t -*-
+(define-package "golden-ratio-scroll-screen" "20221102.240" "Scroll half screen down or up, and highlight current line" 'nil :commit "ed82ac7e9129c7be5983b44def0b9239b54d4dcf" :authors '(("纪秀峰 <jixiuf at gmail dot com>")) :maintainer '("纪秀峰 <jixiuf at gmail dot com>") :keywords '("scroll" "screen" "highlight") :url "https://github.com/jixiuf/golden-ratio-scroll-screen")
---
+++ .emacs.d/elpa/kaolin-themes-20230126.2302/kaolin-themes-pkg.el
@@ -0,0 +1,14 @@
+(define-package "kaolin-themes" "20230126.2302" "A set of eye pleasing themes"
+  '((emacs "25.1")
+    (autothemer "0.2.2")
+    (cl-lib "0.6"))
+  :commit "e69a183c1173f7f9ee19ad06ab109ade3811ae77" :authors
+  '(("Ogden Webb" . "ogdenwebb@gmail.com"))
+  :maintainer
+  '("Ogden Webb" . "ogdenwebb@gmail.com")
+  :keywords
+  '("dark" "light" "teal" "blue" "violet" "purple" "brown" "theme" "faces")
+  :url "https://github.com/ogdenwebb/emacs-kaolin-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
