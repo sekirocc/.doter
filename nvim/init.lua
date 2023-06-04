f37833b2a72e92212ff5427d4e772afd9b194456
chenjie.101 <chenjie.101@bytedance.com>
2023-06-04 22:18:14 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -635,6 +635,21 @@ end
 
 require("clangd_extensions").setup{
     server = {
+          cmd = {
+           "clangd",
+           "-j=4",
+           "--background-index",
+           "--clang-tidy",
+           "--fallback-style=llvm",
+           "--all-scopes-completion",
+           "--completion-style=detailed",
+           "--header-insertion=iwyu",
+           "--header-insertion-decorators",
+           "--pch-storage=memory",
+        },
+        initialization_options = {
+           fallback_flags = { "-std=c++17" },
+        },
         on_attach = my_lsp_on_attach,
         capabilities = get_forced_lsp_capabilities(),
     }
