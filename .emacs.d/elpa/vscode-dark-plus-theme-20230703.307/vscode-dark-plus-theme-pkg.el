b7cbc5ce620e221107b27c7f861fe5c20f25d2d7
Seki Rocc <sekirocc@gmail.com>
2023-07-30 21:54:08 +0800

    vscode-dark-theme

---
+++ .emacs.d/elpa/vscode-dark-plus-theme-20230703.307/vscode-dark-plus-theme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "vscode-dark-plus-theme" "20230703.307" "Default Visual Studio Code Dark+ theme" 'nil :commit "76e0c0859c4ee9d4d6a267cc5ce2f79ba562eddf" :authors
+  '(("Ian Y.E. Pan"))
+  :maintainers
+  '(("Ian Y.E. Pan"))
+  :maintainer
+  '("Ian Y.E. Pan")
+  :url "https://github.com/ianpan870102/vscode-dark-plus-emacs-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
