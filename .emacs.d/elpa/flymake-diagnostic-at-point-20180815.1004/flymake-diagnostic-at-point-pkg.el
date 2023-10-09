ef832de918d6ba974f4385fcad0544e79228bc22
Seki Rocc <sekirocc@gmail.com>
2023-10-09 19:38:49 +0800

    flymake-posframe

--- .emacs.d/elpa/flymake-diagnostic-at-point-20180815.1004/flymake-diagnostic-at-point-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from flymake-diagnostic-at-point.el  -*- no-byte-compile: t -*-
-(define-package "flymake-diagnostic-at-point" "20180815.1004" "Display flymake diagnostics at point" '((emacs "26.1") (popup "0.5.3")) :commit "379616b1c6f5ebeaf08fbe54ae765008a78b3be7" :authors '(("Ricardo Martins" . "ricardo@scarybox.net")) :maintainer '("Ricardo Martins" . "ricardo@scarybox.net") :keywords '("convenience" "languages" "tools") :url "https://github.com/meqif/flymake-diagnostic-at-point")
