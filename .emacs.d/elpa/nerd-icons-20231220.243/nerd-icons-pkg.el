b6e813d688d46e8397be37ead278ee1677287966
Seki Rocc <sekirocc@gmail.com>
2023-12-21 17:24:30 +0800

    update

---
+++ .emacs.d/elpa/nerd-icons-20231220.243/nerd-icons-pkg.el
@@ -0,0 +1,14 @@
+(define-package "nerd-icons" "20231220.243" "Emacs Nerd Font Icons Library"
+  '((emacs "24.3"))
+  :commit "0727a648518dec07755f1e044f6192cf2ced85ab" :authors
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("lisp")
+  :url "https://github.com/rainstormstudio/nerd-icons.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
