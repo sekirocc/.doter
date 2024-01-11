5387e586e6379fbc14864f353e85854b4cf9293b
Seki Rocc <sekirocc@gmail.com>
2024-01-11 12:08:40 +0800

    upgrade all pkgs

---
+++ .emacs.d/elpa/color-theme-sanityinc-tomorrow-20231226.930/color-theme-sanityinc-tomorrow-pkg.el
@@ -0,0 +1,14 @@
+(define-package "color-theme-sanityinc-tomorrow" "20231226.930" "A version of Chris Kempson's \"tomorrow\" themes"
+  '((emacs "24.1"))
+  :commit "a918dfe597e3a350b2dd5368eaf33b0432885453" :authors
+  '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainers
+  '(("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Steve Purcell" . "steve@sanityinc.com")
+  :keywords
+  '("faces" "themes")
+  :url "https://github.com/purcell/color-theme-sanityinc-tomorrow")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
