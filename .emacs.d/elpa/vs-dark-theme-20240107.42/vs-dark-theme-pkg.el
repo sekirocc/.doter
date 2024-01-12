b7e4272f1d98119a7fa7dec603003435be962fd7
Seki Rocc <sekirocc@gmail.com>
2024-01-12 19:31:41 +0800

    book-mode

---
+++ .emacs.d/elpa/vs-dark-theme-20240107.42/vs-dark-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vs-dark-theme" "20240107.42" "Visual Studio IDE dark theme"
+  '((emacs "24.1"))
+  :commit "24298d3683f2357b121984c48120012f124d6f76" :authors
+  '(("Jen-Chieh Shen"))
+  :maintainers
+  '(("Jen-Chieh Shen"))
+  :maintainer
+  '("Jen-Chieh Shen")
+  :url "https://github.com/emacs-vs/vs-dark-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
