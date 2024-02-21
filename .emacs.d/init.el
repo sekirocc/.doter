baa4a903b2624cdd28d375e07938d5e3b4867270
Seki Rocc <sekirocc@gmail.com>
2024-02-21 19:44:52 +0800

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -489,9 +489,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(centaur-tabs-selected ((t (:inherit mode-line))))
+ '(centaur-tabs-selected ((t (:inherit mode-line :inverse-video t))))
  '(centaur-tabs-selected-modified ((t (:inherit centaur-tabs-selected :foreground "pink"))))
- '(centaur-tabs-unselected ((t (:background "#393E44" :foreground "#8F9396"))))
+ '(centaur-tabs-unselected ((t (:inherit mode-line ))))
  '(centaur-tabs-unselected-modified ((t (:inherit centaur-tabs-unselected :foreground "pink"))))
  '(counsel-outline-default ((t (:inherit green))))
  '(deadgrep-match-face ((t (:foreground "#7fdc59" :background "#232d38" :weight normal))))
