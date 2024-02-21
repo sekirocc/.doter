913bf74a6cba76fd16d541db277874ca147d2aad
Seki Rocc <sekirocc@gmail.com>
2024-02-21 19:38:58 +0800

    treesit

---
+++ .emacs.d/elpa/treesit-auto-20240218.1446/treesit-auto-pkg.el
@@ -0,0 +1,14 @@
+(define-package "treesit-auto" "20240218.1446" "Automatically use tree-sitter enhanced major modes"
+  '((emacs "29.0"))
+  :commit "e5a71408d29d3e5796369e3091ecd3f78751c80e" :authors
+  '(("Robb Enzmann" . "robbenzmann@gmail.com"))
+  :maintainers
+  '(("Robb Enzmann" . "robbenzmann@gmail.com"))
+  :maintainer
+  '("Robb Enzmann" . "robbenzmann@gmail.com")
+  :keywords
+  '("treesitter" "auto" "automatic" "major" "mode" "fallback" "convenience")
+  :url "https://github.com/renzmann/treesit-auto.git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
