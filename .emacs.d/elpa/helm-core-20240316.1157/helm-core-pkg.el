8fa6a70449c1f3a1b5bcd67191c8b988f4839ece
Seki Rocc <sekirocc@gmail.com>
2024-03-18 16:33:30 +0800

    upgrade

---
+++ .emacs.d/elpa/helm-core-20240316.1157/helm-core-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-core" "20240316.1157" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "4e4a3889e2b19fe871850254132153b349138f96" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/nerd-icons-20240312.1407/nerd-icons-pkg.el
@@ -0,0 +1,14 @@
+(define-package "nerd-icons" "20240312.1407" "Emacs Nerd Font Icons Library"
+  '((emacs "24.3"))
+  :commit "58db45e003e3c6283db54ace23e3594677ffd050" :authors
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Hongyu Ding <rainstormstudio@yahoo.com>, Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("lisp")
+  :url "https://github.com/rainstormstudio/nerd-icons.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
