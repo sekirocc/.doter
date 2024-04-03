88ccbefcf33d183c7cdac5458bcd68ac292ae811
Seki Rocc <sekirocc@gmail.com>
2024-04-03 11:21:06 +0800

    update

---
+++ .emacs.d/elpa/ivy-xref-20211008.1103/ivy-xref-pkg.el
@@ -0,0 +1,13 @@
+(define-package "ivy-xref" "20211008.1103" "Ivy interface for xref results"
+  '((emacs "25.1")
+    (ivy "0.10.0"))
+  :commit "a82e8e117d2dd62c28b6a3e3d6e4cfb11c0bda38" :authors
+  '(("Alex Murray" . "murray.alex@gmail.com"))
+  :maintainers
+  '(("Alex Murray" . "murray.alex@gmail.com"))
+  :maintainer
+  '("Alex Murray" . "murray.alex@gmail.com")
+  :url "https://github.com/alexmurray/ivy-xref")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
