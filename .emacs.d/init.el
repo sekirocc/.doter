7827648d3a26a62f6e940a8bf3069245b995477f
Seki Rocc <sekirocc@gmail.com>
2024-04-11 18:59:45 +0800

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1574,33 +1574,36 @@ respectively."
 
 
 
-;; (require 'ivy-posframe)
-;; (ivy-posframe-mode 1)
 
-(setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-top-center)))
-(defun ivy-format-function-default (cands)
-  "Transform CANDS into a string for minibuffer."
-  (concat "---------------------------------------------------\n"
-          (ivy--format-function-generic
-           (lambda (str)
-             (concat " > " (ivy--add-face str 'ivy-current-match) ""))
-           (lambda (str)
-             (concat "   " str ""))
-           cands
-           "\n")
-          "\n----------------------------------------------------\n\n"
-          ))
+(use-package ivy-posframe
+  :after (ivy)
+  :config
 
+  (defun ivy-format-function-default (cands)
+    "Transform CANDS into a string for minibuffer."
+    (concat "---------------------------------------------------\n"
+            (ivy--format-function-generic
+             (lambda (str)
+               (concat " > " (ivy--add-face str 'ivy-current-match) ""))
+             (lambda (str)
+               (concat "   " str ""))
+             cands
+             "\n")
+            "\n----------------------------------------------------\n\n"
+            ))
 
-(defun my-ivy-posframe-get-size ()
-  "Set the ivy-posframe size according to the current frame."
-  (let ((height (or ivy-posframe-height (or (+ ivy-height 2) 20)))
-        (width  (or ivy-posframe-width (round (* .80 (frame-width))))))
-    (list :height height :min-height height :min-width width)))
-(setq ivy-posframe-size-function 'my-ivy-posframe-get-size)
-(setq ivy-posframe-parameters '((:internal-border-width . 1) (:internal-border-color . "white")))
+  (defun my-ivy-posframe-get-size ()
+    "Set the ivy-posframe size according to the current frame."
+    (let ((height (or ivy-posframe-height (or (+ ivy-height 2) 20)))
+          (width  (or ivy-posframe-width (round (* .80 (frame-width))))))
+      (list :height height :min-height height :min-width width)))
 
+  (setq ivy-posframe-size-function 'my-ivy-posframe-get-size)
+  (setq ivy-posframe-parameters '((:internal-border-width . 1) (:internal-border-color . "white")))
+  (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-top-center)))
 
+  (ivy-posframe-mode 1)
+)
 
 
 ;; (use-package
@@ -1608,12 +1611,12 @@ respectively."
 ;;   :load-path "~/.emacs.d/lisp/flymake-posframe.el"
 ;;   :hook (flymake-mode . flymake-posframe-mode))
 
-(setq flymake-start-syntax-check-on-find-file nil)
 
 (use-package flymake-diagnostic-at-point
   :load-path "~/.emacs.d/lisp/flymake-diagnostic-at-point.el"
   :after flymake
   :config
+  (setq flymake-start-syntax-check-on-find-file nil)
   (setq flymake-diagnostic-at-point-error-prefix " > ")
   (setq flymake-diagnostic-at-point-display-diagnostic-function 'flymake-diagnostic-at-point-display-posframe)
   (unless (display-graphic-p)
