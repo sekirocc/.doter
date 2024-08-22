4b0f94314d0e313ecc670390447c17386859a770
Seki Rocc <sekirocc@gmail.com>
2024-08-22 16:46:54 +0800

    qml

---
+++ .emacs.d/elpa/company-qml-20170428.1708/company-qml-pkg.el
@@ -0,0 +1,14 @@
+(define-package "company-qml" "20170428.1708" "Company backend for QML files"
+  '((qml-mode "0.1")
+    (company "0.8.12"))
+  :commit "4af4f32a7ad86d86bb9293fb0b675aec513b5736" :authors
+  '(("Junpeng Qiu" . "qjpchmail@gmail.com"))
+  :maintainers
+  '(("Junpeng Qiu" . "qjpchmail@gmail.com"))
+  :maintainer
+  '("Junpeng Qiu" . "qjpchmail@gmail.com")
+  :keywords
+  '("extensions"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
