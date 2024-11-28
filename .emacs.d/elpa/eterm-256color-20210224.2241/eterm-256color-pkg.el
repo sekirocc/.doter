4ecd96447555c73403e82fee17eea0e685ce70df
Seki Rocc <sekirocc@gmail.com>
2024-11-28 16:59:11 +0800

    update color

---
+++ .emacs.d/elpa/eterm-256color-20210224.2241/eterm-256color-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "eterm-256color" "20210224.2241"
+  "Customizable 256 colors for term."
+  '((emacs       "24.4")
+    (xterm-color "1.7")
+    (f           "0.19.0"))
+  :url "https://github.com/dieggsy/eterm-256color"
+  :commit "05fdbd336a888a0f4068578a6d385d8bf812a4e8"
+  :revdesc "05fdbd336a88"
+  :keywords '("faces")
+  :authors '(("Diego A. Mundo" . "dieggsy@pm.me"))
+  :maintainers '(("Diego A. Mundo" . "dieggsy@pm.me")))
