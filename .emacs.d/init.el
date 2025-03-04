00af6d10cbe1b4ea7dbfbad5ba446ec4f9af2a98
Seki Rocc <sekirocc@gmail.com>
2025-03-04 18:57:42 +0800

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -145,7 +145,7 @@
     (setq whitespace-trailing-fg-color "#59728F")
     ;;
     (setq hl-line-bg-color "#1b2129")
-  )
+    )
   (progn
     (setq darker-window-bg-color "#FFFFFF")
     (setq darker-window-fg-color "black")
@@ -231,11 +231,12 @@
 
 
 (load-theme 'doom-xcode t)
+(set-face-attribute 'font-lock-keyword-face nil :weight 'normal)
+(set-face-attribute 'region nil :background "#214283")
 
 
 ;; (require 'xcode-dark-theme)
 ;; (load-theme 'xcode-dark t)
-;; (set-face-attribute 'region nil :background "#214283")
 
 
 
@@ -578,7 +579,7 @@
   '(leetcode-prefer-language "cpp")
   '(leetcode-save-solutions t)
   '(package-selected-packages
-     '(dockerfile-mode popper spacious-padding There afternoon-theme ansible apheleia are atom-one-dark-theme auto-dim-other-buffers bazel beacon benchmark-init blamer centaur-tabs challenger-deep-theme color-theme-sanityinc-tomorrow company-posframe company-prescient company-qml corfu-terminal counsel csv-mode cyberpunk-theme dashboard dashboard-hackernews diminish disable-mouse easy-kill eglot elisp-autofmt elisp-def elisp-refs elisp-slime-nav eterm-256color format-all general google-c-style helm-rg highlight-numbers highlight-parentheses imenu-list impatient-mode inkpot-theme ivy ivy-posframe ivy-xref jdecomp jsonrpc key-chord known leetcode lispy lsp-python-ms markdown-mode markdown-toc markdownfmt modus-themes moe-theme mwim nano-theme neotree nimbus-theme no package-lint paredit phi-search popon popup-switcher popwin prescient project projectsile-mode protobuf-mode py-autopep8 qml-mode rainbow-delimiters reformatter ripgrep rjsx-mode selected slime slime-company smart-jump smart-mode-line smooth-scrolling solaire-mode srcery-theme srefactor swift-mode switch-buffer-functions symbol-overlay treemacs-all-the-icons treesit-auto typescript-mode undo-tree valign vs-dark-theme vscode-dark-plus-theme vterm vue-mode with-proxy with-simulated-input xclip yasnippet))
+     '(poetry dockerfile-mode popper spacious-padding There afternoon-theme ansible apheleia are atom-one-dark-theme auto-dim-other-buffers bazel beacon benchmark-init blamer centaur-tabs challenger-deep-theme color-theme-sanityinc-tomorrow company-posframe company-prescient company-qml corfu-terminal counsel csv-mode cyberpunk-theme dashboard dashboard-hackernews diminish disable-mouse easy-kill eglot elisp-autofmt elisp-def elisp-refs elisp-slime-nav eterm-256color format-all general google-c-style helm-rg highlight-numbers highlight-parentheses imenu-list impatient-mode inkpot-theme ivy ivy-posframe ivy-xref jdecomp jsonrpc key-chord known leetcode lispy lsp-python-ms markdown-mode markdown-toc markdownfmt modus-themes moe-theme mwim nano-theme neotree nimbus-theme no package-lint paredit phi-search popon popup-switcher popwin prescient project projectsile-mode protobuf-mode py-autopep8 qml-mode rainbow-delimiters reformatter ripgrep rjsx-mode selected slime slime-company smart-jump smart-mode-line smooth-scrolling solaire-mode srcery-theme srefactor swift-mode switch-buffer-functions symbol-overlay treemacs-all-the-icons treesit-auto typescript-mode undo-tree valign vs-dark-theme vscode-dark-plus-theme vterm vue-mode with-proxy with-simulated-input xclip yasnippet))
   '(pos-tip-background-color "#1d1d2b")
   '(pos-tip-foreground-color "#d4d4d6")
   '(projectile-globally-ignored-directories
