49a9f9fb22e9a5e7108f9bf503323137b020c974
Seki Rocc <sekirocc@gmail.com>
2025-03-04 19:19:47 +0800

    update

--- .emacs.d/lisp/init-lang-python.el
+++ .emacs.d/lisp/init-lang-python.el
@@ -1,11 +1,18 @@
-(use-package python-ts-mode
-  :defer t
-  :after eglot
-  :config
-  (add-to-list 'eglot-server-programs
-    '((python-mode python-ts-mode) . ("hatch" "run" "pyright-langserver" "--stdio")))
-  (setq python-shell-interpreter "hatch")
-  (setq python-shell-interpreter-args "run ipython -i --simple-prompt --InteractiveShell.display_page=True")
+
+(defun my-python-mode-hook()
+  (eglot-ensure)
   )
+
+(add-hook 'python-ts-mode-hook #'my-python-mode-hook)
+
+
+(use-package poetry
+  :ensure t
+  :defer t
+  :config
+  ;; Checks for the correct virtualenv. Better strategy IMO because the default
+  ;; one is quite slow.
+  (setq poetry-tracking-strategy 'switch-buffer)
+  :hook (python-ts-mode . poetry-tracking-mode))
 
 (provide 'init-lang-python)
