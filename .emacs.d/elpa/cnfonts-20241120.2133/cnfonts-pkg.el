e8b462762f17cbcc5ca4af7a339a29763b4b6419
Bell Top <jie.chen@xme.world>
2025-07-23 14:52:55 +0800

    add cnfonts

---
+++ .emacs.d/elpa/cnfonts-20241120.2133/cnfonts-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "cnfonts" "20241120.2133"
+  "A simple Chinese fonts config tool."
+  '((emacs "24"))
+  :url "https://github.com/tumashu/cnfonts"
+  :commit "0b4fb8c2f743594010bb8137db6e71087efdaf67"
+  :revdesc "0b4fb8c2f743"
+  :keywords '("convenience" "chinese" "font")
+  :authors '(("Feng Shu" . "tumashu@163.com"))
+  :maintainers '(("Feng Shu" . "tumashu@163.com")))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -306,6 +306,16 @@
 ;;; EDITING & TEXT MANIPULATION
 ;;; ===================================================================
 
+
+
+(require 'cnfonts)
+;; 让 cnfonts 在 Emacs 启动时自动生效。
+(cnfonts-mode 1)
+;; 添加两个字号增大缩小的快捷键
+;; (define-key cnfonts-mode-map (kbd "C--") #'cnfonts-decrease-fontsize)
+;; (define-key cnfonts-mode-map (kbd "C-=") #'cnfonts-increase-fontsize)
+
+
 ;;; Basic Editing Enhancements
 (delete-selection-mode 1)
 (column-number-mode 1)
