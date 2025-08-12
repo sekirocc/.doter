5eb681bb42d5f40bed9d8b33ccf3821737c5430e
Chen Jie <jie.chen@xme.world>
2025-08-12 15:15:21 +0800

    update

--- .emacs.d/lisp/init-vterm.el
+++ .emacs.d/lisp/init-vterm.el
@@ -14,11 +14,16 @@
   :config
   (setq vterm-timer-delay 0.001
         vterm-max-scrollback 10000)
-  (define-key vterm-mode-map (kbd "s-C")  #'vterm-copy-mode)
+
+  (define-prefix-command 'my-vterm-ctrl-s-key)
+  (define-key vterm-mode-map (kbd "C-s") 'my-vterm-ctrl-s-key)
+
+  (define-key vterm-mode-map (kbd "C-s [")  #'vterm-copy-mode)
+  (define-key vterm-copy-mode-map (kbd "y")  #'vterm-copy-mode-done)
+  (define-key vterm-copy-mode-map (kbd "M-w")  #'vterm-copy-mode-done)
+
   (define-key vterm-mode-map (kbd "C-s-c")  #'(lambda() (interactive) (vterm-send-key "c" nil nil t)))
   (define-key vterm-mode-map (kbd "M-i")  #'er/expand-region)
-  (define-key vterm-copy-mode-map (kbd "M-w")  #'vterm-copy-mode-done)
-  (define-key vterm-copy-mode-map (kbd "s-C")  #'vterm-copy-mode)
   (add-hook 'vterm-copy-mode-hook #'(lambda() (if (bound-and-true-p vterm-copy-mode)
                                                    (my-special-buffer-keys-minor-mode 1)
                                                  (my-special-buffer-keys-minor-mode 0))))
