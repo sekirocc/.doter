fd499eac55e7d18f274ec1f4b46a5d650878ffc7
Chen Jie <jie.chen@xme.world>
2025-09-15 19:32:47 +0800

    update

--- .emacs.d/lisp/my-key-bindings.el
+++ .emacs.d/lisp/my-key-bindings.el
@@ -60,7 +60,7 @@
 (bind-key* (kbd "s-w") #'kill-current-buffer)
 (bind-key* (kbd "s-t") #'+funcs/new-empty-buffer)
 
-(bind-key* (kbd "C-T") #'multi-vterm)
+(bind-key* (kbd "s-T") #'multi-vterm)
 (bind-key* (kbd "C-t") #'claude-posframe-toggle)
 
 (bind-key* (kbd "s-<backspace>") #'my-delete-to-bol)
