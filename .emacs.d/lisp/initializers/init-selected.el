49d2b089513b3f39b6e10b324e7981baacbd00af
Chen Jie <jie.chen@xme.world>
2025-09-23 11:19:38 +0800

    update

--- .emacs.d/lisp/initializers/init-selected.el
+++ .emacs.d/lisp/initializers/init-selected.el
@@ -31,7 +31,7 @@
            ("(" . my-wrap-region-with-parens)
            ("[" . my-wrap-region-with-brackets)
            ("{" . my-wrap-region-with-braces)
-           ("'" . my-wrap-region-with-single-quotes)
+           ("C-'" . my-wrap-region-with-single-quotes)
            ("\"" . my-wrap-region-with-double-quotes)
            ("_" . my-wrap-region-with-underscores)
            ("`" . my-wrap-region-with-back-quotes)
