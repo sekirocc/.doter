5250b40e07f7210f882f21000e904a07dcfa5760
Chen Jie <jie.chen@xme.world>
2025-09-29 14:25:11 +0800

    update

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -9,10 +9,41 @@ return {
     ft = "python",
   },
 
-  -- Auto pairs
+  -- Auto pairs - Modern Neovim-compatible version
   {
-    "jiangmiao/auto-pairs",
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      local autopairs = require("nvim-autopairs")
+      autopairs.setup({
+        check_ts = true, -- Enable treesitter integration
+        ts_config = {
+          lua = {'string'},-- it will not add a pair on that treesitter node
+          javascript = {'template_string'},
+          java = false,-- don't check treesitter on java
+        },
+        disable_filetype = { "TelescopePrompt", "vim" },
+        fast_wrap = {
+          map = '<M-e>',
+          chars = { '{', '[', '(', '"', "'" },
+          pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+          end_key = '$',
+          keys = 'qwertyuiopzxcvbnmasdfghjkl',
+          check_comma = true,
+          highlight = 'Search',
+          highlight_grey='Comment'
+        },
+      })
+
+      -- Integration with nvim-cmp
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
   },
 
   -- Multiple cursors
